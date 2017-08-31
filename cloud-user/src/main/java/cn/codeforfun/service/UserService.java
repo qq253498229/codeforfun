4461aa9bd50967532e7e5fe2fa10876e07b6a5a1
大胃王 <253498229@qq.com>
2017-08-31 09:37:44 +0800

    删除log

--- cloud-user/src/main/java/cn/codeforfun/service/UserService.java
+++ cloud-user/src/main/java/cn/codeforfun/service/UserService.java
@@ -2,7 +2,6 @@ package cn.codeforfun.service;
 
 import cn.codeforfun.dao.UserDao;
 import cn.codeforfun.entity.User;
-import org.apache.log4j.Logger;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -14,13 +13,10 @@ import javax.annotation.Resource;
 @Service
 @Transactional
 public class UserService {
-  private static final Logger log = Logger.getLogger(UserService.class);
   @Resource
   private UserDao userDao;
 
   public Page<User> findPage(int page, int size) {
-    log.info("page===\r\n" + page);
-    log.info("size===\r\n" + size);
     return userDao.findAll(new PageRequest(page - 1, size));
   }
 }

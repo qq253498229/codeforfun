368d8eb14357f35e42c65cb5ed054aab89b9d09a
大胃王 <253498229@qq.com>
2017-09-01 14:44:47 +0800

    整合eureka

--- cloud-user/src/main/java/cn/codeforfun/UserApplication.java
+++ cloud-user/src/main/java/cn/codeforfun/UserApplication.java
@@ -2,18 +2,20 @@ package cn.codeforfun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class Application {
+@EnableDiscoveryClient
+public class UserApplication {
   @RequestMapping("/")
   public String index() {
     return "user index";
   }
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class);
+    SpringApplication.run(UserApplication.class);
   }
 }
--- cloud-user/src/main/java/cn/codeforfun/dao/UserDao.java
+++ cloud-user/src/main/java/cn/codeforfun/dao/UserDao.java
@@ -2,6 +2,10 @@ package cn.codeforfun.dao;
 
 import cn.codeforfun.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserDao extends JpaRepository<User, Integer> {
+
+  @Query("select u from User u")
+  User findByParam();
 }

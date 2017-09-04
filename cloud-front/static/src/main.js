72c5f2ecc3d6cb81127a0ce8db6ea7cfd383971d
大胃王 <253498229@qq.com>
2017-09-04 15:18:58 +0800

    静态资源改用min的

--- cloud-front/static/src/main.js
+++ cloud-front/static/src/main.js
@@ -3,9 +3,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery/src/jquery'
-import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/js/bootstrap.min'
 import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false;

5151c6c947ec800f59502c249c31d647169cc844
大胃王 <253498229@qq.com>
2017-09-04 16:04:07 +0800

    提取cloud-user配置文件到config-server中

--- cloud-user/src/main/java/cn/codeforfun/UserApplication.java
+++ cloud-user/src/main/java/cn/codeforfun/UserApplication.java
@@ -1,7 +1,7 @@
 package cn.codeforfun;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +16,6 @@ public class UserApplication {
   }
 
   public static void main(String[] args) {
-    SpringApplication.run(UserApplication.class);
+    new SpringApplicationBuilder(UserApplication.class).web(true).run(args);
   }
 }

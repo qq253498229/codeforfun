161bd770c9d7aa42cbb0a7fe2dac628d96b1621f
大胃王 <253498229@qq.com>
2017-11-06 10:31:03 +0800

    update

--- cloud-config/src/main/java/cn/codeforfun/ConfigApplication.java
+++ cloud-config/src/main/java/cn/codeforfun/ConfigApplication.java
@@ -3,12 +3,14 @@ package cn.codeforfun;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author wangbin
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigApplication {
   public static void main(String[] args) {
     new SpringApplicationBuilder(ConfigApplication.class).web(true).run(args);

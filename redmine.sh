ba15ebb4e1096e98be46021f7f7188852cfcaaac
大胃王 <253498229@qq.com>
2017-11-10 16:20:20 +0800

    加入redmine

--- redmine.sh
+++ redmine.sh
@@ -1,3 +1,4 @@
 git pull
-docker-compose -f docker-compose-prod.yml rm -s -f cloud-nginx redmine-mariadb redmine
-docker-compose -f docker-compose-prod.yml up -d cloud-nginx redmine-mariadb redmine
+docker-compose -f docker-compose-prod.yml rm -s -f cloud-nginx redmine
+docker-compose -f docker-compose-prod.yml up -d cloud-nginx redmine
+docker-compose -f docker-compose-prod.yml logs -f redmine

752ecb386bb13a48979621101d02a0004fc84877
大胃王 <253498229@qq.com>
2017-11-17 15:32:57 +0800

    修改单元测试

--- cloud-oauth2-server/src/test/java/cn/codeforfun/test/OauthTest.java
+++ cloud-oauth2-server/src/test/java/cn/codeforfun/test/OauthTest.java
@@ -1,86 +1,100 @@
 package cn.codeforfun.test;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import okhttp3.FormBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import org.apache.commons.codec.binary.Base64;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import javax.annotation.Resource;
+
 @Slf4j
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
 public class OauthTest {
-
-
-
+  @Resource
+  private WebApplicationContext context;
+  @Resource
+  private FilterChainProxy proxy;
+  private MockMvc mvc;
   private String username = "admin";
   private String password = "admin";
   private String clientId = "client";
   private String clientSecret = "secret";
 
-  private String accessToken;
-  private String refreshToken;
-
   @Before
-  public void before() throws IOException {
-    String url = "http://localhost:8110/oauth/token";
-    OkHttpClient client = new OkHttpClient();
-    FormBody.Builder builder = new FormBody.Builder();
-    builder.add("username", username);
-    builder.add("password", password);
-    builder.add("grant_type", "password");
-    byte[] bytes = Base64.encodeBase64((clientId + ":" + clientSecret).getBytes());
-    Request request = new Request.Builder()
-            .url(url)
-            .post(builder.build())
-            .addHeader("Content-Type", "application/x-www-form-urlencoded")
-            .addHeader("Authorization", "Basic " + new String(bytes))
-            .build();
-    Response response = client.newCall(request).execute();
-    Map map = new ObjectMapper().readValue(response.body().string(), HashMap.class);
-    this.accessToken = (String) map.get("access_token");
-    this.refreshToken = (String) map.get("refresh_token");
+  public void before() throws Exception {
+    mvc = MockMvcBuilders.webAppContextSetup(context).addFilter(proxy).build();
+    this.getToken();
   }
 
   @Test
-  public void checkToken() throws IOException {
-    String url = "http://localhost:8110/oauth/check_token";
-    OkHttpClient client = new OkHttpClient();
-    FormBody.Builder builder = new FormBody.Builder();
-    builder.add("token", this.accessToken);
-    Request request = new Request.Builder()
-            .url(url)
-            .post(builder.build())
-            .addHeader("Content-Type", "application/x-www-form-urlencoded")
-            .build();
-    Response response = client.newCall(request).execute();
-    log.info(response.body().string());
-    Assert.assertEquals(200, response.code());
+  public void checkToken() throws Exception {
+    MockHttpServletResponse response = mvc.perform(
+            post("/oauth/check_token")
+                    .param("token", this.getToken().accessToken)
+    ).andReturn().getResponse();
+    Assert.assertEquals(200, response.getStatus());
   }
 
   @Test
-  public void refreshToken() throws IOException {
-    String url = "http://localhost:8110/oauth/token";
-    OkHttpClient client = new OkHttpClient();
-    FormBody.Builder builder = new FormBody.Builder();
-    builder.add("refresh_token", this.refreshToken);
-    builder.add("grant_type", "refresh_token");
-    byte[] bytes = Base64.encodeBase64((clientId + ":" + clientSecret).getBytes());
-    Request request = new Request.Builder()
-            .url(url)
-            .post(builder.build())
-            .addHeader("Content-Type", "application/x-www-form-urlencoded")
-            .addHeader("Authorization", "Basic " + new String(bytes))
-            .build();
-    Response response = client.newCall(request).execute();
-    log.info(response.body().string());
-    Assert.assertEquals(200, response.code());
+  public void refreshToken() throws Exception {
+    MockHttpServletResponse response = mvc.perform(
+            post("/oauth/token")
+                    .param("grant_type", "refresh_token")
+                    .param("refresh_token", getToken().refreshToken)
+                    .header("Authorization", "Basic " + new String(Base64.encodeBase64((clientId + ":" + clientSecret).getBytes())))
+    ).andReturn().getResponse();
+    Assert.assertEquals(200, response.getStatus());
+  }
 
+  public Token getToken() throws Exception {
+    MockHttpServletResponse response = mvc.perform(
+            post("/oauth/token")
+                    .param("username", username)
+                    .param("password", password)
+                    .param("grant_type", "password")
+                    .header("Authorization", "Basic " + new String(Base64.encodeBase64((clientId + ":" + clientSecret).getBytes())))
+    ).andReturn().getResponse();
+    return new ObjectMapper().readValue(response.getContentAsString(), Token.class);
+  }
+
+  private MockHttpServletRequestBuilder get(String url) {
+    return MockMvcRequestBuilders.get(url);
+  }
+
+  private MockHttpServletRequestBuilder post(String url) {
+    return MockMvcRequestBuilders.post(url);
+  }
+
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  private static class Token {
+    @JsonProperty("access_token")
+    public String accessToken;
+    @JsonProperty("refresh_token")
+    public String refreshToken;
+    @JsonProperty("token_type")
+    public String tokenType;
+    @JsonProperty("expires_in")
+    public int expiresIn;
+    @JsonProperty("scope")
+    public String scope;
   }
 }
+
+
